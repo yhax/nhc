a28faa9b04064ca6edb8817bbecc1a3ec690741d
Matt Lagrotte <matt@lagrotte.net>
2026-06-17 10:45:50 -0400

    Add async setup function for unclvito_nhca
    Implement async setup for unclvito_nhca component

---
+++ custom_components/unclvito_nhca/__init__.py
@@ -0,0 +1,14 @@
+import logging
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+_LOGGER = logging.getLogger(__name__)
+DOMAIN = "unclvito_nhca"
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the custom components for NHC Atlantic."""
+    # This forwards the setup process straight to sensor.py
+    hass.async_create_task(
+        hass.helpers.discovery.async_load_platform("sensor", DOMAIN, {}, config)
+    )
+    return True
